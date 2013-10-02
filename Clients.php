2482084409961400e00fd4708af6810760f08c51
Nikolay Bogolubov <nbogol@gmail.com>
2013-10-02 15:43:49 -0400

    Create Clients.php

---
+++ Clients.php
@@ -0,0 +1,18 @@
+ 
+ client_tbl
+ ===========
+ - name
+ - phone, email, address
+ - DOB
+ - employer, address, tel
+ - SSN
+ 
+ Cosigner / Guarantor
+ --------------------
+ - name
+ - phone, email, address
+ - DOB
+ - SSN
+ 
+<?php
+
