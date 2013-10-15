72c08a7268815125d713bae4b8163beafc83734f
Nikolay Bogolubov <nbogol@gmail.com>
2013-10-15 11:40:04 -0400

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -1,6 +1,24 @@
-Jobs:
+Jobs types:
+- SMS - send SMS message(s)
+- EMAIL - send email message(s)
+- PDFGEN - pdf document generation
 
 job_tbl
 -------
 - job_id
-- job_type
+- owner_user_id
+- job_type (SMS, EMAIL, PDFGEN)
+- status (NEW,ACTIVE,COMPLETE,ERROR)
+- schedule (ONE_TIME, REPEAT, ... ?)
+- priority
+- enrty_time
+- start_time
+- compelte_time
+- on_start (<yii component name>.<method>(job_id))
+- on_complete (<yii component name>.<method>(job_id))
+- on_error (<yii component name>.<method>(job_id))
+
+job_pdfgen_detail_tbl
+---------------------
+- job_detail_id
+- job_id
